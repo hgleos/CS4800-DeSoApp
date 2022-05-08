431d0ec9bf1b132e6ae2d656e70d0e7ae72cf45b
Nole Serrano <43283288+NoleSerrano@users.noreply.github.com>
2022-05-08 03:16:37 -0700

    Add files via upload
    Made it so upload images works

--- android_deso_app/lib/Screens/Listings/create_listing_page.dart
+++ android_deso_app/lib/Screens/Listings/create_listing_page.dart
@@ -1,8 +1,14 @@
+import 'dart:io';
+
 import 'package:android_deso_app/Screens/Elements/app_bar.dart';
 import 'package:android_deso_app/Screens/Elements/app_bottom_navigation_bar2.dart';
 import 'package:flutter/material.dart';
 import 'package:android_deso_app/constants.dart';
 import 'package:android_deso_app/Screens/Components/string_extensions.dart';
+import 'dart:async';
+
+import 'package:image_picker/image_picker.dart';
+
 
 class CreateListingPage extends StatefulWidget {
   const CreateListingPage({Key? key}) : super(key: key);
@@ -12,118 +18,159 @@ class CreateListingPage extends StatefulWidget {
 }
 
 class _CreateListingPageState extends State<CreateListingPage> {
-  final _formkey = GlobalKey<FormState>();
+  final ImagePicker _picker = ImagePicker();
+  List<XFile>? _imageFileList = [];
 
+  final _formkey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Form(
       key: _formkey,
       child: Scaffold(
         appBar: desoAppBar(context, true),
-        body: SingleChildScrollView(
-          child: Center(
-            child: Column(
-              children: [
-                Container(
-                  margin: EdgeInsets.only(top: 40),
-                  child: Text(
-                    'Create your listing',
-                    style: boldHeading,
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(top: 20),
-                  child: ElevatedButton(
-                      onPressed: () {},
-                      style: elevatedButtonStyle,
-                      child: Text(
-                        'Upload your images',
-                      )),
-                ),
-                Container(
-                  margin: EdgeInsets.only(top: 20),
-                  child: TextFormField(
-                    maxLength: 200,
-                    textInputAction: TextInputAction.next,
-                    validator: (s) {
-                      if (s!.isEmpty) {
-                        return 'This is a required field.';
-                      }
-                    },
-                    style: TextStyle(fontSize: 20),
-                    decoration: const InputDecoration(
-                      helperText: '',
-                      hintText: 'title',
+          body: Center(
+            child: SafeArea(
+              child: Column(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(top: 40),
+                    child: Text(
+                      'Create your listing',
+                      style: boldHeading,
                     ),
                   ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(top: 5),
-                  child: TextFormField(
-                    keyboardType: TextInputType.numberWithOptions(
-                        decimal: true, signed: false),
-                    textInputAction: TextInputAction.next,
-                    validator: (s) {
-                      if (s!.isEmpty) {
-                        return 'This is a required field.';
-                      }
-                      if (!s!.isValidDouble()) {
-                        return 'Enter a valid price.';
-                      }
-                    },
-                    style: TextStyle(fontSize: 20),
-                    decoration: const InputDecoration(
-                      helperText: '',
-                      hintText: 'price',
-                    ),
+                  Container(
+                    margin: EdgeInsets.only(top: 20),
+                    child: ElevatedButton(
+                        onPressed: () {
+                          _formkey.currentState!.validate();
+
+                        },
+                        style: elevatedButtonStyle,
+                        child: Text(
+                          'Publish your listing',
+                        )),
                   ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(top: 5),
-                  child: TextFormField(
-                    textInputAction: TextInputAction.next,
-                    validator: (s) {
-                      if (s!.isEmpty) {
-                        return 'This is a required field.';
-                      }
-                    },
-                    style: TextStyle(fontSize: 20),
-                    decoration: const InputDecoration(
-                      helperText: '',
-                      hintText: 'tags',
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(top: 5),
-                  child: TextFormField(
-                    maxLines: 6,
-                    maxLength: 1000,
-                    textInputAction: TextInputAction.next,
-                    style: TextStyle(fontSize: 20),
-                    decoration: const InputDecoration(
-                      helperText: '',
-                      hintText: 'description',
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(top: 15, bottom: 10),
-                  child: ElevatedButton(
-                      onPressed: () {
-                        _formkey.currentState!.validate();
+
+                  Container(
+                    margin: EdgeInsets.only(top: 20),
+                    child: TextFormField(
+                      maxLength: 200,
+                      textInputAction: TextInputAction.next,
+                      validator: (s) {
+                        if (s!.isEmpty) {
+                          return 'This is a required field.';
+                        }
                       },
-                      style: elevatedButtonStyle,
-                      child: Text(
-                        'Publish your listing',
-                      )),
-                ),
-              ],
+                      style: TextStyle(fontSize: 20),
+                      decoration: const InputDecoration(
+                        helperText: '',
+                        hintText: 'title',
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 5),
+                    child: TextFormField(
+                      keyboardType: TextInputType.numberWithOptions(
+                          decimal: true, signed: false),
+                      textInputAction: TextInputAction.next,
+                      validator: (s) {
+                        if (s!.isEmpty) {
+                          return 'This is a required field.';
+                        }
+                        if (!s!.isValidDouble()) {
+                          return 'Enter a valid price.';
+                        }
+                      },
+                      style: TextStyle(fontSize: 20),
+                      decoration: const InputDecoration(
+                        helperText: '',
+                        hintText: 'price',
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 5),
+                    child: TextFormField(
+                      maxLines: 2,
+                      maxLength: 1000,
+                      textInputAction: TextInputAction.next,
+                      style: TextStyle(fontSize: 20),
+                      decoration: const InputDecoration(
+                        helperText: '',
+                        hintText: 'description',
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 15, bottom: 10),
+                    child: ElevatedButton(
+                        onPressed: () {
+                          selectImages();
+
+                        },
+                        style: elevatedButtonStyle,
+                        child: Text(
+                          'Upload your images',
+                        )),
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(20),
+                      child: GridView.builder(
+                          itemCount: _imageFileList!.length,
+                          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+                          itemBuilder: (BuildContext context, int index) {
+                            return Padding(
+                              padding: const EdgeInsets.all(2.0),
+                              child: Stack(
+                                fit: StackFit.expand,
+                                children: [Image.file(
+                                  File(_imageFileList![index].path),
+                                  fit: BoxFit.cover,
+                                ),
+                                  Positioned(
+                                    right: -5,
+                                      top: -5,
+                                      child: IconButton(
+iconSize: 30,
+                                        color: Colors.red,
+                                        icon: Icon(Icons.delete),
+                                        onPressed: () {
+                                          setState(() {
+                                            _imageFileList!.removeAt(index);
+
+                                          });
+                                        },
+                                      ),
+
+                                  )
+                                ],
+
+                              ),
+                            );
+                          }),
+                    ),
+                  )
+
+                ],
+              ),
             ),
           ),
-        ),
-        bottomNavigationBar: AppBottomNavBar2(),
+
       ),
     );
+  }
+
+
+  void selectImages() async{
+    final List<XFile>? selectedImages = await _picker.pickMultiImage();
+    if (selectedImages!.isNotEmpty) {
+      _imageFileList!.addAll(selectedImages);
+    }
+    setState(() {
+
+    });
   }
 }
